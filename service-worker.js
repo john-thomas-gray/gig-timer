04bca74bd236ebf6b93372956678916ca5a02c87
John <john.thom.gray@gmail.com>
2025-12-09 17:56:17 -0500

    recognizeUrls

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,14 @@
-import { doesUrlMatch } from "./js/urlUtils.js";
-
-console.log("Service worker starting...");
-
 const storageCache = { count: 0, urls: {} };
 
+// Doesn't go fast enough
 async function initStorageCache() {
   const items = await chrome.storage.sync.get(["count", "lastTabId", "urls"]);
   Object.assign(storageCache, items);
 }
-//never runs
+initStorageCache();
+
+//never runs because popup blocks action
 chrome.action.onClicked.addListener(async (tab) => {
-  console.log("init storage");
-  await initStorageCache();
   storageCache.count++;
   storageCache.lastTabId = tab.id;
   chrome.storage.sync.set({
@@ -22,31 +19,26 @@ chrome.action.onClicked.addListener(async (tab) => {
 
 chrome.storage.onChanged.addListener((changes, area) => {
   if (area === "sync" && changes.urls?.newValue) {
-    console.log("XXXXX",changes.urls.newValue)
     storageCache.urls = changes.urls.newValue;
   }
 });
 
 chrome.webNavigation.onCompleted.addListener(
   (details) => {
-    const { tabId, url } = details;
-    console.log("Details object:", storageCache);
+    const { frameId, url } = details;
+    if (frameId !== 0) return;
+
     const { assignments, workplace } = storageCache.urls;
-    console.log(
-      "assignments",
-      assignments,
-      "currentUrl",
-      url,
-      "equal?",
-      assignments == url,
-      "doesUrlMatch",
-      doesUrlMatch(assignments, url)
-    );
-    if (doesUrlMatch(assignments, url)) {
+
+    if (!assignments && !workplace) return;
+
+    // something more robust. normalization
+    if (url.includes(assignments)) {
       console.log("Get assignment details");
-    } else if (doesUrlMatch(workplace, url)) {
+    } else if (url.includes(workplace)) {
       console.log("Start timer");
     }
   },
-  { url: [{ hostContains: "google.com" }, { hostContains: "frogger.com" }] }
+  // how can I make this dynamic
+  { url: [{ hostContains: "localization.pixelogicmedia.com" }] }
 );
